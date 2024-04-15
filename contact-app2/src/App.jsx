549aca0f0d75238d7c458672d6d67fbbbc10a27f
Umuratwa <umuratwar@gmail.com>
2024-04-15 17:35:05 +0200

    my contact app

--- contact-app2/src/App.jsx
+++ contact-app2/src/App.jsx
@@ -1,15 +1,21 @@
-import { useState } from 'react'
-import Header from './component/header'
-import './App.css'
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AddPage from "./pages/AddPage";
+import DisplayPage from "./pages/DisplayPage";
+import UpdatePage from "./pages/UpdatePage";
+import UserInfo from "./pages/UserInfo"
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-   <div>
-    <Header/>
-   </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AddPage />} />
+        <Route path="/contacts" element={<DisplayPage />} />
+        {/* Update the route to accept a parameter */}
+        <Route path="/update/:contactId" element={<UpdatePage />} />
+        {/* Update the route to accept a parameter */}
+        <Route path="/details/:contactId" element={<UserInfo />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App
+export default App;
---
+++ contact-app2/src/pages/AddPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AddForm from "../components/AddForm"
+
+const AddPage = () => {
+  return (
+    <div className="container mx-auto mt-10  justify-center w-96 items-center border border-gray-100 rounded-lg p-4 bg-white  shadow-lg ">
+      <h1 className="text-2xl font-bold mb-5">ADD CONTACT FORM</h1>
+      <AddForm />
+    </div>
+  );
+}
+
+export default AddPage
